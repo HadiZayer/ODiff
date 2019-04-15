3e78cfa29fa0aff6307d4a814a2298ce1b096c99
VaRaj <varun19.rajesh@gmail.com>
2019-04-15 19:57:20 -0400

    Add files via upload

--- AutoDif.ml
+++ AutoDif.ml
@@ -1,5 +1,5 @@
 module Diff = struct
-  type 'a diff = {value: float; children : 'a diff list; 
+  type diff = {value: float; children : diff list; 
   op: float list; cur_grad : float; grad : float}
   let get_value dif = dif.value
   let get_children dif = dif.children
@@ -7,6 +7,6 @@ module Diff = struct
   let get_cur_grad dif = dif.cur_grad
   let add = failwith "Unimplemented"
   let mul = failwith "Unimplemented"
-  let create_var = failwith "Unimplemented"
+  let init x = failwith "Unimplemented"
   let print = failwith "Unimplemented"
   end
--- AutoDif.mli
+++ AutoDif.mli
@@ -1,8 +1,14 @@
 module type Diff = sig
-  type 'a diff (* var *)
-  val get_value : 'a dif -> float
-  val get_children : 'a dif -> 'a dif list
-  val get_op : 'a dif list -> float list
-  val get_cur_grad : 'a dif -> float
-  val get_grad : 'a dif -> float
+  type diff (* var *)
+  val get_value : diff -> float
+  val get_children : diff -> diff list
+  val get_op : diff list -> float list
+  val get_cur_grad : diff -> float
+  val get_grad : diff -> float
+  val init : float -> diff
+  (* val add : 
+  val mul : 
+  val create_var : 
+  val print : 
+  *)
   end
