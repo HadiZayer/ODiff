ff3ec8818fc0532b63b11780c96e6a068063b0c0
HadiZayer <hmzayer@hotmail.com>
2019-04-16 16:00:47 -0400

    implemented operation functions

--- AutoDif.ml
+++ AutoDif.ml
@@ -1,12 +1,22 @@
 module Diff = struct
-  type diff = {value: float; children : diff list; 
-  op: float list; cur_grad : float; grad : float}
+  type diff = {value: float; children : diff array; 
+  op: diff array -> float array; mutable cur_grad : float; mutable grad : float}
   let get_value dif = dif.value
   let get_children dif = dif.children
   let get_op dif = dif.op
   let get_cur_grad dif = dif.cur_grad
-  let add = failwith "Unimplemented"
-  let mul = failwith "Unimplemented"
-  let init x = failwith "Unimplemented"
+  let add arg0 arg1 =
+    let add_grad children = [|1.0;1.0|] in
+    let v = arg0.value +. arg1.value in
+    {value=v; children=[|arg0;arg1|]; op=add_grad; cur_grad=0.0; grad=0.0}
+  let mul arg0 arg1 =
+    let mul_grad children = 
+      assert (Array.length children = 2);
+      [|children.(1).value; children.(0).value|] in
+    let v=arg0.value *. arg1.value in
+    {value=v; children=[|arg0; arg1|]; op=mul_grad; cur_grad=0.0;grad=0.0}
+  let init x = 
+    let id _ = [||] in
+    {value=x; children=[||]; op=id; cur_grad=0.0;grad=0.0}
   let print = failwith "Unimplemented"
   end
